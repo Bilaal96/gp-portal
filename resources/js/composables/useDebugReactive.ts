a95db4c2d30a53126814426a305be2c1de308332
Bilaal96 <bilaal-ahmad@hotmail.co.uk>
2026-04-21 16:49:39 +0100

    feat: add composable helpers to debug (watch & log) reactive() state

---
+++ resources/js/composables/useDebugReactive.ts
@@ -0,0 +1,32 @@
+import { watch } from 'vue';
+
+export function useDebugReactive<T extends Record<string, unknown>>(
+    reactiveState: T,
+    label: string = 'useDebugReactive',
+) {
+    for (const property in reactiveState) {
+        watch(
+            () => reactiveState[property],
+            (value) => {
+                console.log(label, {
+                    [property]: JSON.parse(JSON.stringify(value)),
+                });
+            },
+        );
+    }
+}
+
+export function useDebugReactiveProp<T extends Record<string, unknown>>(
+    reactiveState: T,
+    property: keyof T,
+    label: string = 'useDebugReactiveProp:',
+) {
+    watch(
+        () => reactiveState[property],
+        (value) => {
+            console.log(label, {
+                [property]: JSON.parse(JSON.stringify(value)),
+            });
+        },
+    );
+}
